4cc60afeac652b0fdd562ffd00f8563703520560
Jakub Černík <jakubcernik15@gmail.com>
2025-04-08 15:39:59 +0200

    final version

--- Lecture6/spotEvaluator.py
+++ Lecture6/spotEvaluator.py
@@ -12,10 +12,13 @@ total_predictions = 0
 
 def extract_lbp_features(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    lbp = local_binary_pattern(gray, n_points, radius, method="uniform")
+
+    # Noise
+    denoised = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    lbp = local_binary_pattern(denoised, n_points, radius, method="uniform")
     histogram = np.histogram(lbp.ravel(), bins=n_points + 2, range=(0, n_points + 2))[0]
     return histogram / histogram.sum()
-
 
 def train_classifier():
     X_train, y_train = [], []
@@ -43,53 +46,49 @@ for i in range(1, 25):
 
     predictions = []
     for slot in slots_data:
-        # Získáme body parkovacího místa jako pole
         body_mista = np.array(slot['points'], dtype='float32')
 
-        # Definujeme cílové body obdélníkového tvaru
+        # Define the target rectangle
         cilove_body = np.array([
-            [0, 0],                         # levý horní
-            [slot_width - 1, 0],            # pravý horní
-            [slot_width - 1, slot_height - 1], # pravý dolní
-            [0, slot_height - 1]            # levý dolní
+            [0, 0],                             # left top
+            [slot_width - 1, 0],                # right top
+            [slot_width - 1, slot_height - 1],  # right bottom
+            [0, slot_height - 1]                # left bottom
         ], dtype='float32')
 
-        # Transformujeme místo do standardní velikosti
+        # Transform to rectangle
         transformace = cv2.getPerspectiveTransform(body_mista, cilove_body)
         normalizovane_misto = cv2.warpPerspective(image, transformace, (slot_width, slot_height))
 
-        # Extrahujeme příznaky a predikujeme stav (0=volno, 1=obsazeno)
-        priznaky = extract_lbp_features(normalizovane_misto)
-        predikce = clf.predict([priznaky])[0]
+        # Predict using classifier (0=free, 1=full)
+        features = extract_lbp_features(normalizovane_misto)
+        predikce = clf.predict([features])[0]
         predictions.append(predikce)
 
-    # Vykreslení predikce
+    # Draw prediction
     for slot_idx, slot in enumerate(slots_data):
-        # Převést body slotu na čísla typu int32
         body_slotu = np.array(slot['points'], dtype='int32')
 
-        # Určit barvu podle predikce (zelená = volno, červená = obsazeno)
         if predictions[slot_idx] == 0:
-            barva = (0, 255, 0)  # zelená = volno
+            barva = (0, 255, 0)  # free
         else:
-            barva = (0, 0, 255)  # červená = obsazeno
+            barva = (0, 0, 255)  # full
 
-        # Vykreslit polygon kolem parkovacího místa
+        # Draw the polygon
         cv2.polylines(image, [body_slotu], True, barva, 2)
 
     cv2.imshow("Vyhodnocene parkoviste", image)
     cv2.waitKey(1000)
     cv2.destroyAllWindows()
 
-    # Uložení výsledků
-    # Otevření souboru pro zápis výsledků
+    # Saving results
     output_file = open(f'test_results_zao/result_{i}.txt', 'w')
-    # Zápis každé predikce na samostatný řádek
+
     for prediction in predictions:
         output_file.write(str(prediction) + '\n')
     output_file.close()
 
-    # Vyhodnocení přesnosti
+    # Precision evaluation
     with open(f'test_texts_zao/test{i}.txt', 'r') as f_true:
         ground_truth = []
         for line in f_true.readlines():
